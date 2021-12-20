9fece63d7a303f9a2c3098d348c939ebfb5a7f48
Cecilia Daninthe <cecilia.daninthe@gmail.com>
2021-12-20 13:45:43 -0500

    commit tree 0ae5114ef6f7d26e5393c275721326338fd12a84

--- index.js
+++ index.js
@@ -1,2 +1,27 @@
-
 // Write your code here
+let num1 = 31;
+let num2 = 31;
+let multiply = num1 * num2;
+if (multiply = 62){
+    multiply;
+}
+
+
+let random;
+random = Math.floor(Math.random()*10)+1;
+random;
+
+
+let num3=16;
+let num4=6;
+let mod = num3 % num4;
+if (mod=4){
+    mod;
+}
+
+
+let max = Math.max(2, 4, 6, 8, 10, 12, 14, 16, 18, 20);
+
+
+
+
